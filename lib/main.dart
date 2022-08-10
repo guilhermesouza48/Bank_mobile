4b14234e194669f5b6a145f0a7c88752cf0fea26
FlutLab.io <>
2022-08-10 00:27:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       // A widget which will be started on application startup
       home: MyHomePage(title: 'Meu primeiro Olá mundo!'),
