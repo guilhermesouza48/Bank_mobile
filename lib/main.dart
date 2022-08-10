b62d1872f729a796ff112b4d01f862dc29921f61
FlutLab.io <>
2022-08-10 01:03:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Column(
       children: const <Widget>[
-        Text('Olá textando',textDirection: TextDirection.ltr,),
-        Text('1...2...3',textDirection: TextDirection.ltr,),
+        Text(
+          'Olá textando',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          '1...2...3',
+          textDirection: TextDirection.ltr,
+        ),
         Expanded(
           child: FittedBox(
             fit: BoxFit.contain, // otherwise the logo will be tiny
