ea5c95bdfa65a9705b841c4fd600a8d40b765b91
FlutLab.io <>
2022-08-16 23:02:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        //Header
         home: Scaffold(
           appBar: AppBar(
             title: Text('HOME'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add)
           ),
         ),
       ),
