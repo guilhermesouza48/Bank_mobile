5db764c73feca3d21fdbbe29e2c30cf7f1825ab4
FlutLab.io <>
2022-08-16 22:49:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('HOME'),
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('HOME'),
+          ),
+        ),
+      ),
+    );
