f9a176bb72c227e7144f7dd580171f1fdeb72cbc
FlutLab.io <>
2022-09-14 00:50:21 +0000

    13/09 - SEPARAÇÕES EM PASTAS

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/transferencias/lista_transferencia.dart';
 
-void main() => runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            //Header
-            title: Text('HOME'),
-            backgroundColor: Colors.red,
-          ),
-          // Main
-          body: ListaTransf(),
+void main() => runApp(Banco());
 
-          floatingActionButton: FloatingActionButton(
-            // button add
-            onPressed: () {},
-            child: Icon(Icons.add),
-            backgroundColor: Colors.red,
-          ),
-        ),
-      ),
-    );
-
-// CLASS QUE MOSTRA A LISTA
-class ListaTransf extends StatelessWidget {
+class Banco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // CRIAR COLUNAS DE TEXTOS - CHILDREN
-      children: [
-        ItemTransf('200.0', 'SUA MAE'),
-      ],
-    );
-  }
-}
-
-// CLASS ITEM QUE ESTÁ DENTRO DA LISTA
-class ItemTransf extends StatelessWidget {
-
-  final String valor;
-  final String conta;
-
-  ItemTransf(this.valor, this.conta);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      // CRIA LINHAS DE TEXTOS
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(conta),
-      ),
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListaTransferencia(),
     );
   }
 }
---
+++ lib/screens/transferencias/formulario_transferencia.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import '../../components/editor.dart';
+import '../../models/transferencia.dart';
+
+const _tituloAppBar = 'Criando transferência';
+
+const _rotuloCampoNumeroConta = 'Numero da conta';
+const _dicaCampoNumeroConta = '0000';
+
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTranferenciaState();
+  }
+}
+
+class FormularioTranferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: _rotuloCampoNumeroConta,
+              dica: _dicaCampoNumeroConta,
+            ),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: _rotuloCampoValor,
+              dica: _dicaCampoValor,
+              icone: Icons.monetization_on,
+            ),
+            ElevatedButton(
+              child: Text(_textoBotaoConfirmar),
+              onPressed: () {
+                _criaTransferencia(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    debugPrint('clicou em confirmar');
+    final int? numeroConta = int.tryParse(
+      _controladorCampoNumeroConta.text,
+    );
+    final double? valor = double.tryParse(
+      _controladorCampoValor.text,
+    );
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      debugPrint('Criando Transferência');
+      debugPrint('$transferenciaCriada');
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
